cc3ea94b7b8fa4825a2fdae8842cef26c08442e5
uttamsakle1 <78955341+uttamsakle1@users.noreply.github.com>
2021-02-15 12:11:58 +0530

    Initial commit of math.py file

---
+++ math.py
@@ -0,0 +1,12 @@
+#add implimentation
+def add(x,y):
+    pass
+ #sub implimentation  
+def subtract(x,y):
+    pass
+ #muliply implimentation
+def multiply(x,y):
+    pass
+ # divide implimentation   
+def divide(x,y):
+    pass   
